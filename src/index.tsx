bfc4df7f8bd19c470662d98ca3a3d54c3d4c220d
whiskacz <pawelblasiak1985@gmail.com>
2023-09-13 20:29:50 +0200

    redux + ts practice app

--- src/index.tsx
+++ src/index.tsx
@@ -1,19 +1,8 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import { createRoot } from 'react-dom/client';
 
-const root = ReactDOM.createRoot(
-  document.getElementById('root') as HTMLElement
-);
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+function Ts7 () {
+    return <div>Hejka</div>
+}
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+const root = createRoot(document.getElementById('root')!)
+root.render(<Ts7 />)
---
+++ src/state/store.ts
@@ -0,0 +1,6 @@
+import { configureStore } from "@reduxjs/toolkit";
+import { applyMiddleware} from 'redux'
+import thunk from 'redux-thunk';
+import reducers from './reducers';
+
+export const store = configureStore(reducers, {}, applyMiddleware(thunk))
